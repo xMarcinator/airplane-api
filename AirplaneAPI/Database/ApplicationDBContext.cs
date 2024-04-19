ca50d916bec09fe7bce919a0bc0e5572e5843008
Marc Jense <marcjensenvirklund@gmail.com>
2024-04-19 21:36:41 +0200

    Added seeding to context

--- AirplaneAPI/Database/ApplicationDBContext.cs
+++ AirplaneAPI/Database/ApplicationDBContext.cs
@@ -1,3 +1,4 @@
+using AirplaneAPI.Database.DTO;
 using AirplaneAPI.Database.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,66 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public required DbSet<Models.Airplane> Airplanes { get; set; }
     public required DbSet<Models.Flight> Flights { get; set; }
     public required DbSet<Models.Airport> Airports { get; set; }
+
+    public async override Task OnSeed(ILogger<Program> logger)
+    {
+        var Airplane = await Airplanes.AddAsync(new Airplane
+        {
+            Model = "Boeing 747",
+            Manufacturer = "Boeing",
+            Capacity = 416,
+            Range = 14815,
+            MaxSpeed = 988,
+            PlaneWeight = 162400,
+            MaxLiftOffWeight = 333400,
+            Length = 70,
+            Height = 19,
+            Wingspan = 64,
+            Price = 357000000,
+            MotorType = MotorType.Jet,
+            PlaneType = PlaneType.Passenger,
+            PilotCount = 2,
+            PassangerCount = 416,
+            FuelCapacity = 183380,
+            FuelConsumption = 12000
+        });
+
+        var firstAirport =await Airports.AddAsync(new Airport()
+        {
+            Name = "Schiphol",
+            City = "Amsterdam",
+            Country = "Netherlands",
+            IATA = "AMS",
+            Latitude = 52.308056,
+            Longitude = 4.764167,
+            Timezone = 1,
+        });
+        
+        var secondAirport = await Airports.AddAsync(new Airport()
+        {
+            Name = "Heathrow",
+            City = "London",
+            Country = "United Kingdom",
+            IATA = "LHR",
+            Latitude = 51.4775,
+            Longitude = -0.461389,
+            Timezone = 0,
+        });
+        
+        await Flights.AddAsync(new Flight()
+        {
+            DepartureAirport = firstAirport.Entity,
+            ArrivalAirport = secondAirport.Entity,
+            DepartureTime = DateTime.Now,
+            ArrivalTime = DateTime.Now.AddHours(1),
+            Airplane = Airplane.Entity,
+            FlightNumber = "KL1234",
+            Distance = 500,
+            FuelCost = 1000
+        });
+        
+        await SaveChangesAsync();
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
